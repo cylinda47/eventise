b32a7b939bcef4d27aaa4aff9bd996baded58cc4
Linda Chan <cylinda0407@gmail.com>
2017-11-22 23:46:18 -0800

    Commit before experiment with rte

--- frontend/components/event_form/event_form.jsx
+++ frontend/components/event_form/event_form.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import RichTextEditor from 'react-rte';
 import { Redirect } from 'react-router-dom';
 
-export default class EventForm extends React.Component {
+class EventForm extends React.Component {
+    
     constructor(props){
         super(props);
+        this.propTypes = { onChange: PropTypes.func};
+        this.value = { value: RichTextEditor.createEmptyValue() };
         this.state = this.props.formType === "create" ?
-            Object.assign({}, this.props.event)
+            Object.assign({}, this.value, this.props.event)
             :
-            Object.assign({ id: '' }, this.props.event)
+            Object.assign({ id: '' }, this.value, this.props.event)
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    static propTypes = {
+        onChange: PropTypes.func
     }
 
     componentWillMount() {
         // Redirect if :eventId/edit doesnt exist
     }
+
+    handleInput() {
+
+    }
+
+    handleAddress() {
+
+    }
+
+    onInputChange(value) {
+        this.setState({ value });
+        if (this.props.onChange) {
+            // Send the changes up to the parent component as an HTML string.
+            // This is here to demonstrate using `.toString()` but in a real app it
+            // would be better to avoid generating a string on each change.
+            this.props.onChange(
+                value.toString('html')
+            );
+        }
+    };
 
     render(){
         return(
@@ -21,7 +51,7 @@ export default class EventForm extends React.Component {
                     <div className="header-section-box">1</div>
                     <h1>Event Details</h1>
                 </div>
-                <div className={event-form-box}>
+                <div className="event-form-box">
                     <label>
                         Event Title
                         <input type="text" value={this.state.title} onChange={this.handleInput} />
@@ -30,8 +60,30 @@ export default class EventForm extends React.Component {
                         Location
                         <input type="text" value={this.state.address} onChange={this.handleAddress} />
                     </label>
+                    <label>
+                        Start Date
+                        <input type="date" value={this.state.startDate} onChange={this.handleInput}/>
+                    </label>
+                    <label>
+                        Event Description
+                         <RichTextEditor
+                            value={this.state.value}
+                            onChange={this.onInputChange}
+                            className="rich-text-editer"
+                        />
+                    </label>
+                    <label>
+                  
+                    </label>
                 </div>
             </div>
         )
     }
+
 }
+
+EventForm.propTypes = {
+    onChange: PropTypes.func
+}
+
+export default EventForm;
