7de408d69089cc3e223bd6ff81932f9aed98343e
Linda Chan <cylinda0407@gmail.com>
2017-12-01 02:21:24 -0800

    Add webpack script

--- frontend/components/category/category_bar.jsx
+++ frontend/components/category/category_bar.jsx
@@ -9,13 +9,13 @@ export default class CategoryBar extends React.Component {
     render() {
         return(
                 <div className="scrollmenu">
-                    <Link to="/category/music"><div><img src="assets/categories/music.jpeg" /><label>Music</label></div></Link>
-                    <Link to="/category/food_drink"><div><img src="assets/categories/food_drink.jpeg" /><label>Food & Drink</label></div></Link>
-                    <Link to="/category/classes"><div><img src="assets/categories/classes.jpeg" /><label>Classes</label></div></Link>
-                    <Link to="/category/arts"><div><img src="assets/categories/arts.jpeg" /><label>Arts</label></div></Link>
-                    <Link to="/category/parties"><div><img src="assets/categories/parties.jpeg" /><label>Parties</label></div></Link>
-                    <Link to="/category/sports_wellness"><div><img src="assets/categories/sports.jpeg" /><label>Sports</label></div></Link>
-                    <Link to="/category/networking"><div><img src="assets/categories/networking.jpeg" /><label>Networking</label></div></Link>
+                <Link to="/category/music"><div><img src="https://images.unsplash.com/photo-1491848128688-e0c30289e89f?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Music</label></div></Link>
+                <Link to="/category/food_drink"><div><img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Food & Drink</label></div></Link>
+                <Link to="/category/classes"><div><img src="https://images.unsplash.com/photo-1504194377022-9e04c123352f?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Classes</label></div></Link>
+                <Link to="/category/arts"><div><img src="https://images.unsplash.com/photo-1506625744989-c4ac05e0117a?auto=format&fit=crop&w=2850&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Arts</label></div></Link>
+                <Link to="/category/parties"><div><img src="https://images.unsplash.com/photo-1496843916299-590492c751f4?auto=format&fit=crop&w=1651&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Parties</label></div></Link>
+                <Link to="/category/sports_wellness"><div><img src="https://images.unsplash.com/photo-1472108482137-8df36ccf0d7b?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Sports</label></div></Link>
+                <Link to="/category/networking"><div><img src="https://images.unsplash.com/photo-1490111718993-d98654ce6cf7?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" /><label>Networking</label></div></Link>
                 </div>
         )
     }
--- frontend/components/event_index/search_background.jsx
+++ frontend/components/event_index/search_background.jsx
@@ -20,12 +20,15 @@ export default class SearchBackground extends React.Component {
 
     render(){
         return(
-            <div className="search-background-container">
-                <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1483919283443-8db97e2bcd81?auto=format&fit=crop&w=1650&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
-                <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1492739159057-7d1896b3c63f?auto=format&fit=crop&w=1789&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
-                <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1429277096327-11ee3b761c93?auto=format&fit=crop&w=1651&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
-                <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1494707924465-e1426acb48cb?auto=format&fit=crop&w=1350&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
-                <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1466150036782-869a824aeb25?auto=format&fit=crop&w=1650&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />    
+            <div className="search-header">
+                <div className="search-background-container">
+                    <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1466150036782-869a824aeb25?auto=format&fit=crop&w=1650&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />    
+                    <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?auto=format&fit=crop&w=1651&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
+                    <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1454442124827-b7de573f10e0?auto=format&fit=crop&w=1651&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
+                    <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1455734729978-db1ae4f687fc?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
+                    <img className="mySlides w3-animate-fading" src="https://images.unsplash.com/photo-1505373877841-8d25f7d46678?auto=format&fit=crop&w=1600&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" style={{width: '100%'}} />
+                </div>
+                <center><p>Find your next experience</p></center>
             </div>
         )
     }
